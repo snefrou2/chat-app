2560ecec085cd9f7eaf090b7e373637ec2f79d49
sophie <sneuville59@gmail.com>
2020-03-31 17:19:58 +0200

    ajout composant

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,12 +8,12 @@ import './contact.css';
 const Contact = props => {
     return (
         <div className ="Contact">
-            <img src = {props.user.avatar} className="avatar" alt ="avatar"/>
+            <img src = {props.avatar} className="avatar" alt ="avatar"/>
             <div>
-                <h4 className= "name">{props.user.name}</h4> 
+                <h4 className= "name">{props.name}</h4> 
                 <div className = "status">
-                <span className = {props.user.online ? 'status-online': 'status-offline'}></span>
-                <p className = "status-text">{ props.user.online ? "online":"offline"}</p>   
+                <span className = {props.online ? 'status-online': 'status-offline'}></span>
+                <p className = "status-text">{ props.online ? "online":"offline"}</p>   
                 </div>
             </div>
         </div>
@@ -23,7 +23,7 @@ const Contact = props => {
   
 
  Contact.propTypes = {
-    characterFirstName: PropTypes.string.isRequired,
-    characterLastName: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    online: PropTypes.bool,
   };
   export default Contact;
