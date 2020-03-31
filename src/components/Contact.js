27fa6c1da5123aa8787fdab3d7b1808e14890eef
sophie <sneuville59@gmail.com>
2020-03-31 16:07:54 +0200

    modif avec props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,29 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import './contact.css';
 
-const personnage = {
-    name : "Arielle Mermaid",
-    avatar : 'https://media.cultura.com/media/catalog/product/cache/1/image/1000x1000/9df78eab33525d08d6e5fb8d27136e95/c/a/canevas-12x12-ariel-8435282505738_1.jpg?t=1509811988',
-    status : false
-}
 
 
-function Contact() {
+
+const Contact = props => {
     return (
         <div className ="Contact">
-            <img src = {personnage.avatar} className="avatar"/>
+            <img src = {props.user.avatar} className="avatar" alt ="avatar"/>
             <div>
-                <h4 className= "name">{personnage.name}</h4> 
+                <h4 className= "name">{props.user.name}</h4> 
                 <div className = "status">
-                    <p className = "status-texte">{ personnage.status ? "online":"offline"} { personnage.status ? 
-                    <span className= "status-online"></span>:<span className= "status-offline"></span>}</p>
-                   
+                <span className = {props.user.online ? 'status-online': 'status-offline'}></span>
+                <p className = "status-text">{ props.user.online ? "online":"offline"}</p>   
                 </div>
             </div>
         </div>
       
     );
   }
+  
+
+ Contact.propTypes = {
+    characterFirstName: PropTypes.string.isRequired,
+    characterLastName: PropTypes.string.isRequired,
+  };
   export default Contact;
