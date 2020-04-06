ba2360911fb8b30cab7449aa76e49ceb19abb9fe
sophie <sneuville59@gmail.com>
2020-04-06 13:45:30 +0200

    suppression des online dans le contactlist et ajout d'un event pour passer de online/offline quand clic sur voyant ou texte

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,33 @@ import React from 'react';
 import './contact.css';
 
 
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            online:false,
+        };
+    }
 
-const Contact = props => {
+  render() {  return (
+        <div className ="Contact">
+            <img src = {this.props.avatar} className="avatar" alt ="avatar"/>
+            <div>
+                <h4 className= "name">{this.props.name}</h4> 
+                <div className = "status" onClick={event => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+              }}>
+                <span className={this.state.online ? 'status-online' : 'status-offline'}></span>
+                <p className = "status-text">{ this.state.online ? "online":"offline"}</p>   
+                </div>
+            </div>
+        </div>
+      
+    );
+  }
+}
+/*const Contact = props => {
     return (
         <div className ="Contact">
             <img src = {props.avatar} className="avatar" alt ="avatar"/>
@@ -18,7 +43,7 @@ const Contact = props => {
         </div>
       
     );
-  }
+  }*/
   
 
  Contact.propTypes = {
