7234e53bbc49ade7d417e71bf4fe4b9a572a527e
sophie <sneuville59@gmail.com>
2020-04-06 09:22:07 +0200

    modif exo 3 react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import Users from './components/ContactList'
 import './App.css';
-import Contact from './components/Contact'
-import { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import	List from './components/List'
+
 
 
 function App() {
   return (
     <div className="App">
-      {Users.map ((user, i) => {
-        return < Contact user name = {user.name}
-        avatar = {user.avatar}
-        online = {user.online}
-        key = {i}/>})
-      }
+  < List />
     </div>
   );
 }
 
 export default App;
+
+
