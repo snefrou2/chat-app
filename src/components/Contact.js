58e171ef0e136e9a40085fa82a567d233a5273ea
sophie <sneuville59@gmail.com>
2020-04-06 12:12:13 +0200

    suppression photo en double

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,31 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './contact.css';
 
+class Contact extends React.Component {
+    constructor(props){
+        super(props)
+        this.state= {
+            online:false,
+        }
+    }
+    render() {
+        return (
+        <div className ="Contact">
+            <img src = {this.props.avatar} className="avatar" alt ="avatar"/>
+            <div>
+                <h4 className= "name">{this.props.name}</h4> 
+                <div className = "status">
+                <span className = {this.props.online ? 'status-online': 'status-offline'}></span>
+                <p className = "status-text">{ this.props.online ? "online":"offline"}</p>   
+                </div>
+            </div>
+        </div>
+      
+    );
+  }
+}
 
-
-
-const Contact = props => {
+/*const Contact = props => {
     return (
         <div className ="Contact">
             <img src = {props.avatar} className="avatar" alt ="avatar"/>
@@ -20,7 +41,7 @@ const Contact = props => {
         </div>
       
     );
-  }
+  }*/
   
 
  Contact.propTypes = {
